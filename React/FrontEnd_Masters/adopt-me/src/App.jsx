0b5f6cc5a4ff86d09a39aae869bd07a941a8c938
Ramananda Samantaray <ramanandas@asiapac.altair.com>
2024-02-21 23:08:51 +0530

    Updates Added

--- React/FrontEnd_Masters/adopt-me/src/App.jsx
+++ React/FrontEnd_Masters/adopt-me/src/App.jsx
@@ -1,18 +1,31 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/query-core";
 import "./App.css";
 import SearchParams from "./components/SearchParams";
 import Details from "./components/Details";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  }
+});
+
+
 function App() {
   return (
     <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
       <Link to="/">
-        <header>Adopt Me!</header>
+        <header><h1>Adopt Me!</h1></header>
       </Link>
       <Routes>
         <Route path="/" element={<SearchParams />} />
         <Route path="/details/:id" element={<Details />} />
       </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
