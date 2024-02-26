ebed3779286bfb3744f3fb4feabf86132bc9f683
Ramananda Samantaray <ramanandas@asiapac.altair.com>
2024-02-26 12:41:15 +0530

    gh

--- React/FrontEnd_Masters/adopt-me/src/App.jsx
+++ React/FrontEnd_Masters/adopt-me/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/query-core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import SearchParams from "./components/SearchParams";
 import Details from "./components/Details";
--- React/FrontEnd_Masters/adopt-me/src/components/Details.jsx
+++ React/FrontEnd_Masters/adopt-me/src/components/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 const Details = () => {
   const { id } = useParams();
+  const results = useQuery(["details", id], )
   return (
     <div>
       <h2>{id}</h2>
