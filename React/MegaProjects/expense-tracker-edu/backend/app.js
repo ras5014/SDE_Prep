ae864bbfe33edd8d2303c0a591c189eaa49b0819
Ram <ramanandasamantaray@gmail.com>
2024-04-23 18:52:08 +0530

    Added

--- React/MegaProjects/expense-tracker-edu/backend/app.js
+++ React/MegaProjects/expense-tracker-edu/backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+// const serviceAccount = require("./serviceAccountKey.json");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
